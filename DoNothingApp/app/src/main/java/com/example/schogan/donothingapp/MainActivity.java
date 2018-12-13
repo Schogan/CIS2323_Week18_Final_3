41fbb2845d29e38af33f299fed484e6e8e56d6b5
Schogan <navy15@gmail.com>
2018-12-12 20:07:27 -0600

    complete

--- DoNothingApp/app/src/main/java/com/example/schogan/donothingapp/MainActivity.java
+++ DoNothingApp/app/src/main/java/com/example/schogan/donothingapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.schogan.donothingapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void displayText(View view){
+        // Get the text view
+        TextView displayToSay = (TextView)
+                findViewById(R.id.textView_preview);
+        // Get the value of the text view
+        String toSayString = displayToSay.getText().toString();
+        // Add text to string
+        //toSayString = toSayString + ' ' + "Red";
+        //Convert value to a number and increment it
+        //Integer count = Integer.parseInt(countString);
+        //count++;
+        //Display the new value in the text view
+        Context context = getApplicationContext();
+        CharSequence text = toSayString;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        //displayToSay.setText(toSayString);
     }
 }
